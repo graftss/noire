d17f5b4de3029858ba97b697872529e4efe2ead9
Nick Fireman <nfireman1@gmail.com>
2019-12-09 17:09:21 -0500

    Updates

--- src/editor/components/ComponentEditor/ComponentTitle.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-interface ComponentTitleProps {
-  label: string;
-}
-
-export const ComponentTitle: React.SFC<ComponentTitleProps> = ({ label }) => (
-  <span>{label}</span>
-);
--- src/editor/components/layout/Section.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-export const Section: React.SFC<{}> = ({ children }) => (
-  <div className="editor-section">{children}</div>
-);
